0093c91183810c6c81cf9b3dbb8ac436ad333167
jiang.wang <jiang.wang@opay-inc.com>
2022-10-26 16:26:49 +0800

    pkg: lock-version
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 5174,
+		host: '127.0.0.1'
+	}
 };
 
 export default config;
